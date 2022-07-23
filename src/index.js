d748a616814c33a53608eacc096fd63171531d24
komsomolochka <stupidsoap@gmail.com>
2022-07-23 19:50:39 +0300

    add: solution

--- src/index.js
+++ src/index.js
@@ -38,8 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let exprList = expr.match(/.{1,10}/g);
+    let result = [];
+    let symbolList = [];
+    let symbol;
+    for (let i = 0; i < exprList.length; i++) {
+      let word = exprList[i].match(/.{1,2}/g);
+      let symbolWord = '';
+      for (let j = 0; j < word.length; j++) {
+        if (word[j] === '10') {
+          symbolWord = symbolWord + '.';
+        } else if (word[j] === '11') {
+          symbolWord = symbolWord + '-';
+        }
+      }
+      if (typeof MORSE_TABLE[symbolWord] === 'undefined') {
+        result[i] = ' ';
+      } else {
+        result[i] = MORSE_TABLE[symbolWord];
+      }
+      // write your solution here
+    }
+    return result.join('');
+  }
+  
 
 module.exports = {
     decode
